9cb0314301f3a57885e65a6e1648018578dcc248
Exerlin <61707004+Exerlin@users.noreply.github.com>
2020-09-10 15:08:10 -0400

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+    luhn_verified(credit_card_number) that returns the string
+    “Authentic” if the card is real or “Fake” if the specified
+    credit_card_number is not genuine.
+'''
+def luhn_verified(credit_card_number):
+    
+'''
+    is_valid(sequence) that returns true
+    if the specified string sequence is a valid credit card number, and false
+    otherwise. 
+'''
+def is_valid(sequence):
+    
